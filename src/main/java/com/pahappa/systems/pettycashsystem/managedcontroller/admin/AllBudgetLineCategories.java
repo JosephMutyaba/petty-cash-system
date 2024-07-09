54cee5d04f2ad9cb986d619c3a625a5913c9f6a2
Joseph <josephsteinson@gmail.com>
2024-07-09 10:04:32 +0300

    unfinished add new budgetline dialog

---
+++ src/main/java/com/pahappa/systems/pettycashsystem/managedcontroller/admin/AllBudgetLineCategories.java
@@ -0,0 +1,34 @@
+package com.pahappa.systems.pettycashsystem.managedcontroller.admin;
+
+import com.pahappa.systems.pettycashsystem.spring.models.BudgetLineCategory;
+import com.pahappa.systems.pettycashsystem.spring.services.BudgetLineCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+@Component
+public class AllBudgetLineCategories implements Serializable {
+    @Autowired
+    private BudgetLineCategoryService budgetLineCategoryService;
+
+    private List<BudgetLineCategory> categories;
+    @PostConstruct
+    public void init() {
+        categories = budgetLineCategoryService.getAllBudgetLineCategories();
+    }
+
+    public List<BudgetLineCategory> getCategories() {
+        return budgetLineCategoryService.getAllBudgetLineCategories();
+    }
+
+    public void setCategories(List<BudgetLineCategory> categories) {
+        this.categories = categories;
+    }
+}
