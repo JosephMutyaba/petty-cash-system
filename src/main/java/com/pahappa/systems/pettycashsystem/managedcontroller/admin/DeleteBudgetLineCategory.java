bdd98a1bcfef45a6a7dc5b8b6f3792111f06a1d8
Joseph <josephsteinson@gmail.com>
2024-07-11 20:42:53 +0300

    backend for budgetline categories finished

---
+++ src/main/java/com/pahappa/systems/pettycashsystem/managedcontroller/admin/DeleteBudgetLineCategory.java
@@ -0,0 +1,44 @@
+package com.pahappa.systems.pettycashsystem.managedcontroller.admin;
+
+import com.pahappa.systems.pettycashsystem.spring.models.BudgetLineCategory;
+import com.pahappa.systems.pettycashsystem.spring.services.BudgetLineCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+@Component
+public class DeleteBudgetLineCategory implements Serializable {
+    @Autowired
+    BudgetLineCategoryService budgetLineCategoryService;
+
+    @Autowired
+    private AllBudgetLineCategories allCategories;
+
+    private BudgetLineCategory budgetLineCategory;
+
+    @PostConstruct
+    public void init() {
+        budgetLineCategory = new BudgetLineCategory();
+    }
+
+    public void selectBudgetLineCategory(BudgetLineCategory selectedBudgetLineCategory) {
+        this.budgetLineCategory = selectedBudgetLineCategory;
+    }
+
+    public void deleteBudgetLineCategory() {
+        budgetLineCategoryService.deleteBudgetLineCategory(budgetLineCategory.getId());
+        allCategories.init();
+    }
+
+    public void deleteAllBudgetLineCategories() {
+        budgetLineCategoryService.deleteAllCategories();
+        allCategories.init();
+    }
+
+}
