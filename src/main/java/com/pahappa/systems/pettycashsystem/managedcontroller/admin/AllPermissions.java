ca0902445380874b32cc0eabd91089e7863e2118
DanielHuey <adhueywilan@gmail.com>
2024-07-12 21:29:09 +0300

    missed this file

---
+++ src/main/java/com/pahappa/systems/pettycashsystem/managedcontroller/admin/AllPermissions.java
@@ -0,0 +1,18 @@
+package com.pahappa.systems.pettycashsystem.managedcontroller.admin;
+
+import com.pahappa.systems.pettycashsystem.spring.enums.Permission;
+import org.springframework.stereotype.Component;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+@Named
+@ApplicationScoped
+@Component
+public class AllPermissions {
+    public static final Set<Permission> PERMISSIONS = new HashSet<>(EnumSet.allOf(Permission.class));
+    Set<Permission> selectedPermissions = EnumSet.noneOf(Permission.class);
+}
