6cd871d3a251e381f2a810ddd758e2af0eca3be4
Joseph <josephsteinson@gmail.com>
2024-07-12 16:46:15 +0300

    perms testing

---
+++ src/main/java/com/pahappa/systems/pettycashsystem/spring/enums/Perm.java
@@ -0,0 +1,47 @@
+package com.pahappa.systems.pettycashsystem.spring.enums;
+
+public enum Perm {
+
+
+    add_employee("Add Employee"),
+    delete_employee("Delete Employee"),
+    edit_employee("Edit Employee"),
+
+    add_role("Add Role"),
+    delete_role("Delete Role"),
+    edit_role("Edit Role"),
+
+    make_requisition("Make Requisition"),
+    approve_requisition("Approve Requisition"),
+    reject_requisition("Reject Requisition"),
+    edit_requisition("Edit Requisition"),
+    delete_requisition("Delete Requisition"),
+
+    make_budgetLine("Make BudgetLine"),
+    approve_budgetLine("Approve BudgetLine"),
+    reject_budgetLine("Reject BudgetLine"),
+    edit_budgetLine("Edit BudgetLine"),
+    delete_budgetLine("Delete BudgetLine"),
+
+    view_employees("View Employees"),
+    view_roles("View Roles"),
+    view_requisitions("View Requisitions"),
+    view_budgetLines("View BudgetLines"),
+    view_permissions("View Permissions"),
+
+    request_changes("Request Changes");
+
+    public final String definition;
+
+    Perm(String definition) {
+        this.definition = definition;
+    }
+
+//    boolean hasPermission(String... perms) {
+//    x= CurrentUser.permissions
+//    for permission in x:
+//      for item in perms:
+//          if x.tostring.equals(item) return true;
+//    return false;
+//    }
+}
