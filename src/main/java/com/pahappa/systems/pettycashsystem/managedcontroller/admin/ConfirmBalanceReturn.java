c0dc417b94fe370625cca9edc2438c0fa415e060
Joseph <josephsteinson@gmail.com>
2024-07-28 13:55:07 +0300

    Adding the accountability balance back to to the bugetline balnce after being cleared by the finance

--- src/main/java/com/pahappa/systems/pettycashsystem/managedcontroller/admin/ConfirmBalanceReturn.java
+++ src/main/java/com/pahappa/systems/pettycashsystem/managedcontroller/admin/ConfirmBalanceReturn.java
@@ -1,13 +1,20 @@
 package com.pahappa.systems.pettycashsystem.managedcontroller.admin;
 
+import com.pahappa.systems.pettycashsystem.exceptions.IncompatibleDatesException;
+import com.pahappa.systems.pettycashsystem.exceptions.NullFieldException;
 import com.pahappa.systems.pettycashsystem.spring.models.Accountability;
+import com.pahappa.systems.pettycashsystem.spring.models.BudgetLine;
 import com.pahappa.systems.pettycashsystem.spring.models.Requisition;
 import com.pahappa.systems.pettycashsystem.spring.models.User;
 import com.pahappa.systems.pettycashsystem.spring.services.AccountabilityService;
+import com.pahappa.systems.pettycashsystem.spring.services.BudgetLineService;
+import com.pahappa.systems.pettycashsystem.spring.services.RequisitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -18,6 +25,15 @@ import java.io.Serializable;
 public class ConfirmBalanceReturn implements Serializable {
     @Autowired
     private AccountabilityService accountabilityService;
+
+    @Autowired
+    private RequisitionService requisitionService;
+
+    @Autowired
+    private BudgetLineService budgetLineService;
+
+    @Autowired
+    private AllBudgetLines allBudgetLines;
 
     private Boolean balanceReturned;
 
@@ -82,10 +98,45 @@ public class ConfirmBalanceReturn implements Serializable {
     }
 
 
-
     public void updateAccountabilityBalanceReturnStatus() {
-        accountability.setBalanceIsReturned(balanceReturned);
-        accountabilityService.updateAccountability(accountability);
-        this.balanceReturned=false;
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (balanceReturned) {
+            Requisition req= requisitionService.getRequisitionByAccountabilityId(accountability.getId());
+            BudgetLine budgetLine = req.getBudgetline();
+
+            // START OF ACCOUNTING
+            // add the balance returned to the budgetLine balance
+            Double currentBudgetLineBalance = budgetLine.getBalance();
+            Double accountabilityBalance = req.getAmountGranted() - accountability.getAmountSpent();
+
+            Double newBudgetLineBalance = currentBudgetLineBalance + accountabilityBalance;
+
+            budgetLine.setBalance(newBudgetLineBalance);
+            // END OF ACCOUNTING
+
+            try {
+                //updating thr budgetLine balance in the db
+                budgetLineService.updateBudgetLine(budgetLine);
+
+                // updating the budgetLine table in the budgetLine page
+                allBudgetLines.update();
+
+                accountability.setBalanceIsReturned(balanceReturned);
+
+                // updating the accountability status in the db
+                accountabilityService.updateAccountability(accountability);
+
+                // clear bean values
+                this.balanceReturned=false;
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"cleared successfully", null));
+            } catch (Exception e) {
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Something went wrong, please try again",null));
+                context.validationFailed();
+            }
+
+        }else {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Not cleared", "Not cleared"));
+        }
     }
 }
